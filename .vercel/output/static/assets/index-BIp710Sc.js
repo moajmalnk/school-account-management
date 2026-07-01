698cc6ad0c1817a2c0219d3b901aed95d64d1c8b
moajmalnk <moajmalnk@gmail.com>
2026-07-01 23:11:05 +0530

    u pdated by nk

--- .vercel/output/static/assets/index-BIp710Sc.js
+++
@@ -1,1 +0,0 @@
-import{u as t,r as a}from"./index-LsvZkn3d.js";function r(){const e=t();return a.useEffect(()=>{e({to:"/tenant/dashboard",replace:!0})},[e]),null}export{r as component};
--- .vercel/output/static/assets/index-DPBp65hZ.js
+++
@@ -1,1 +0,0 @@
-import{u as r,r as t}from"./index-LsvZkn3d.js";function a(){const e=r();return t.useEffect(()=>{e({to:"/super-admin/overview",replace:!0})},[e]),null}export{a as component};
